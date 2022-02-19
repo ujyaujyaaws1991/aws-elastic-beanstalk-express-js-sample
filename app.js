f8f1bd0c9f7a6284dda7617c5b4f4af6d26ae38b
ujyaujyaaws1991 <ujyaujyaaws1991@gmail.com>
2022-02-19 15:21:58 +0900

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Yusakueto Githubtestテストだお'));
+app.get('/', (req, res) => res.send('美波さん行ってらっしゃい'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
